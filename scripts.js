7cb0d52f99e3a22d95c5cbd8b83db00d2338a32f
JasonWandragLCA <jason@lifechoices.co.za>
2021-10-26 22:08:23 +0200

    Taken from my codepen

---
+++ scripts.js
@@ -0,0 +1,135 @@
+// Get access to CSS Variables
+let cssVariables = document.querySelector(":root");
+// Set random number for picsum.photos to prevent image caching
+let randomNum = 1;
+
+function changeColor() {
+  let colorPicked = document.getElementById("colorPicker").value;
+  cssVariables.style.setProperty("--color", colorPicked);
+  cssVariables.style.setProperty("--shadowOne", pSBC(-0.2, colorPicked));
+  cssVariables.style.setProperty("--shadowTwo", pSBC(0.05, colorPicked));
+  cssVariables.style.setProperty("--glassColor", addAlpha(colorPicked, 0.5));
+}
+
+function changeTheme() {
+  randomNum++;
+  let el = document.getElementsByClassName("container")[0];
+  if (el.classList.contains("neumorphic")) {
+    el.classList.replace("neumorphic", "glassmorphic");
+  } else {
+    el.classList.replace("glassmorphic", "neumorphic");
+  }
+  cssVariables.style.setProperty(
+    "--glassBackground",
+    `url(https://picsum.photos/1920?random=${randomNum})`
+  );
+}
+
+// COLOR JS STUFF TO GENERATE COLORS. FOR MORE INFO GO HERE: https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+
+const pSBC = (p, c0, c1, l) => {
+  let r,
+    g,
+    b,
+    P,
+    f,
+    t,
+    h,
+    i = parseInt,
+    m = Math.round,
+    a = typeof c1 == "string";
+  if (
+    typeof p != "number" ||
+    p < -1 ||
+    p > 1 ||
+    typeof c0 != "string" ||
+    (c0[0] != "r" && c0[0] != "#") ||
+    (c1 && !a)
+  )
+    return null;
+  if (!this.pSBCr)
+    this.pSBCr = (d) => {
+      let n = d.length,
+        x = {};
+      if (n > 9) {
+        ([r, g, b, a] = d = d.split(",")), (n = d.length);
+        if (n < 3 || n > 4) return null;
+        (x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4))),
+          (x.g = i(g)),
+          (x.b = i(b)),
+          (x.a = a ? parseFloat(a) : -1);
+      } else {
+        if (n == 8 || n == 6 || n < 4) return null;
+        if (n < 6)
+          d =
+            "#" +
+            d[1] +
+            d[1] +
+            d[2] +
+            d[2] +
+            d[3] +
+            d[3] +
+            (n > 4 ? d[4] + d[4] : "");
+        d = i(d.slice(1), 16);
+        if (n == 9 || n == 5)
+          (x.r = (d >> 24) & 255),
+            (x.g = (d >> 16) & 255),
+            (x.b = (d >> 8) & 255),
+            (x.a = m((d & 255) / 0.255) / 1000);
+        else
+          (x.r = d >> 16), (x.g = (d >> 8) & 255), (x.b = d & 255), (x.a = -1);
+      }
+      return x;
+    };
+  (h = c0.length > 9),
+    (h = a ? (c1.length > 9 ? true : c1 == "c" ? !h : false) : h),
+    (f = this.pSBCr(c0)),
+    (P = p < 0),
+    (t =
+      c1 && c1 != "c"
+        ? this.pSBCr(c1)
+        : P
+        ? { r: 0, g: 0, b: 0, a: -1 }
+        : { r: 255, g: 255, b: 255, a: -1 }),
+    (p = P ? p * -1 : p),
+    (P = 1 - p);
+  if (!f || !t) return null;
+  if (l)
+    (r = m(P * f.r + p * t.r)),
+      (g = m(P * f.g + p * t.g)),
+      (b = m(P * f.b + p * t.b));
+  else
+    (r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5)),
+      (g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5)),
+      (b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5));
+  (a = f.a),
+    (t = t.a),
+    (f = a >= 0 || t >= 0),
+    (a = f ? (a < 0 ? t : t < 0 ? a : a * P + t * p) : 0);
+  if (h)
+    return (
+      "rgb" +
+      (f ? "a(" : "(") +
+      r +
+      "," +
+      g +
+      "," +
+      b +
+      (f ? "," + m(a * 1000) / 1000 : "") +
+      ")"
+    );
+  else
+    return (
+      "#" +
+      (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0))
+        .toString(16)
+        .slice(1, f ? undefined : -2)
+    );
+};
+
+// CONVERT HEX COLOR TO HEX WITH OPACITY
+function addAlpha(color, opacity) {
+  // coerce values so ti is between 0 and 1.
+  const _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+  return color + _opacity.toString(16).toUpperCase();
+}
